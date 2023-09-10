46493f5883b1b11db595cd03b5e80724e5937aa1
mr-eyes <mabuelanin@gmail.com>
2023-09-09 21:59:24 -0700

    implement cli

--- src/extract_errors/__init__.py
+++ src/extract_errors/__init__.py
@@ -1,1 +1,26 @@
-from ._extract_errors_impl import *
+from ._extract_errors_impl import HashesCounter
+
+import argparse
+
+
+
+
+def cli():
+    
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--ksize", type=int, default=31)
+    argparser.add_argument("--num-threads", type=int, default=1)
+    argparser.add_argument("sig_paths", nargs="+")
+    args = argparser.parse_args()
+    
+    hashes_counter = HashesCounter(
+        kSize=args.ksize,
+        sig_paths = args.sig_paths,
+        num_threads = args.num_threads
+    )
+    
+    hashes_counter.process()
+
+    error_kmers = hashes_counter.get_error_hashes()
+
+    print(len(error_kmers))
